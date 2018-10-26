77e90ba02cbd1d27c2f18220b8bc8a90816158f4
SRP-STD\j.simon <johann.simon@showroomprive.net>
2018-10-26 09:16:10 +0200

    add info

--- WS_DotNetCore_WebAPI.webApi/Controllers/ValuesController.cs
+++ WS_DotNetCore_WebAPI.webApi/Controllers/ValuesController.cs
@@ -12,6 +12,28 @@ namespace WS_DotNetCore_WebAPI.webApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+
+        /*
+          * créer les dll business et repo
+          * ajouter les dépendances
+          * ajouter dapper ou entity (dapper = bcp mieux :p ) sur repo et web
+          * ajouter les autres nugets comme bootstrap, jquery, blablabla... 
+          * créer la classe BaseRepository qui permet de récup les donnée de connexion et exécuter les requête 
+          * le tout de façon async pour avoir des meilleurs perf (ce qu'on ne fait pas ici ... )
+          * ajouter la connexion à sql dans le appsetting (spécific à core 2.0)
+          * créer la class TaskResult comme entitie qui stock les différent retour d'action
+          * 
+          * remplire les classes business repo (sans oublier les interfaces)
+          * cheminement : vue=>model=>controller=>manager=>repository=>manager=>controller=>model=>vue (le tout en passant par les interfaces
+          * 
+          * Penser a faire le maping des interface (uniquement pour core 2.0 )  dans le startup.cs sinon marche po
+          * 
+          * Voilà tu peux fauire un test de charge à 5 000 000 requête seconde, tu pete même pas de timeout (enfin peut être que la bdd crash mais le site lui tiendra :) )
+          * 
+          * pour l'asp.net framework c'est pareil juste moins performant, et tu risque de faire péter iis à 5 000 000 req / seconde
+          * 
+          * */
+
         private readonly IRechercheTextManager _rechercheTextManager;
         public ValuesController(IRechercheTextManager rechercheTextManager)
         {
